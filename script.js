f95d2d4f6e8c5540d4721db51ffadb714a9723dd
Amachineche <amaphilosophy@gmail.com>
2024-07-24 07:00:56 +0100

    i declared my varaible in js

---
+++ script.js
@@ -0,0 +1,5 @@
+let mortgageAmount = document.getElementsByClassName("Amount");
+let MortgageTerm = document.getElementsByClassName("Year");
+let interestRate= document.getElementsByClassName("interest");
+let calRepaymentBtn = document.getElementsByClassName("cal-repayment-button");
+let resultShow = document.getElementsByClassName("result");
